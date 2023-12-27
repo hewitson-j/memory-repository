869c087e615ed485b1a27e0c035d009838c647ae
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-27 11:36:25 -0700

    Added header description.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,9 @@ export default function Header() {
   return (
     <div className="header">
       <h1>Memory Repository</h1>
+      <h2>
+        A repository for Hewitson and Roberg family photos and scrapbooks.
+      </h2>
     </div>
   );
 }
