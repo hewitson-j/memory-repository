ef4a23eea03e71107dff8b352099f9cc156eead3
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-28 11:26:35 -0700

    Added links.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Links from "./Links";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Links />
     </>
   );
 }
---
+++ src/components/Links.tsx
@@ -0,0 +1,13 @@
+import "./Links.css";
+
+import { Link } from "react-router-dom";
+
+export default function Links() {
+  return (
+    <div className="links">
+      <Link to={"/"} title="Home">
+        <button>Home</button>
+      </Link>
+    </div>
+  );
+}
