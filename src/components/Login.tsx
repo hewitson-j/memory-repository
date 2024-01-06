95126a9986d04cb6d2b1a83192cf6ac486d5196b
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-01-06 11:10:09 -0700

    Added sign in handler.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,40 @@
 import { useState } from "react";
 import "./Login.css";
+import supabase from "../supabaseconfig";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const [inputType, setInputType] = useState("password");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSignIn = async () => {
+    if (username.trim() === "" || password.trim() === "") {
+      alert(
+        "Username and password cannot be blank. \n\nPlease type in both username and password."
+      );
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: username,
+      password: password,
+    });
+    if (error) {
+      alert("Error: " + error);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+      alert(`Welcome ${data.user.email}!`);
+      navigate("home");
+    }
+  };
 
   return (
     <div className="login">
@@ -31,30 +62,48 @@ export default function Login() {
           <label className="login-labels" htmlFor="password-field">
             Password:
           </label>
-          <input
-            form="login-form"
-            type="password"
-            placeholder={"Password"}
-            id="password-field"
-            required
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
+          <div className="password-container">
+            <input
+              form="login-form"
+              type={inputType}
+              placeholder={"Password"}
+              id="password-field"
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <button
+              className="login-buttons"
+              onClick={(e) => {
+                e.preventDefault();
+                inputType === "password"
+                  ? setInputType("text")
+                  : setInputType("password");
+              }}
+            >
+              {inputType === "password" ? "Show Password" : "Hide Password"}
+            </button>
+          </div>
           <button
             form="login-form"
             className="login-buttons"
             type="submit"
-            onClick={() => {
-              console.log(username, password);
-            }}
+            onClick={handleSignIn}
           >
-            Submit
+            {isLoading ? "Signing in..." : "Submit"}
           </button>
         </form>
         <div className="sign-up">
           <h3>Don't have an account?</h3>
-          <button className="login-buttons">Sign up!</button>
+          <button
+            className="login-buttons"
+            onClick={() => {
+              window.location.href = "mailto:jacob.a.hewitson@gmail.com";
+            }}
+          >
+            Sign up!
+          </button>
         </div>
       </div>
     </div>
