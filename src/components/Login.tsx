80c9c0c5ddf27f86a6e96ca21ea303502a06dce3
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-01-06 10:10:32 -0700

    Added styling and elements for login, added sign out button in navbar.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,45 +7,55 @@ export default function Login() {
 
   return (
     <div className="login">
-      <div>
+      <div className="login-content">
         <div className="login-header">
           <h1>Memory Library</h1>
           <h2>
             A repository for Hewitson and Roberg family photos and scrapbooks.
           </h2>
         </div>
-        <label className="login-labels" htmlFor="username-field">
-          Username:
-        </label>
-        <input
-          type="text"
-          placeholder={"Email Address"}
-          id="username-field"
-          required
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <label className="login-labels" htmlFor="password-field">
-          Password:
-        </label>
-        <input
-          type="password"
-          placeholder={"Password"}
-          id="password-field"
-          required
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button
-          type="submit"
-          onClick={() => {
-            console.log(username, password);
-          }}
-        >
-          Submit
-        </button>
+        <form className="login-form">
+          <label className="login-labels" htmlFor="username-field">
+            Username:
+          </label>
+          <input
+            type="text"
+            form="login-form"
+            placeholder={"Email Address"}
+            id="username-field"
+            required
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <label className="login-labels" htmlFor="password-field">
+            Password:
+          </label>
+          <input
+            form="login-form"
+            type="password"
+            placeholder={"Password"}
+            id="password-field"
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button
+            form="login-form"
+            className="login-buttons"
+            type="submit"
+            onClick={() => {
+              console.log(username, password);
+            }}
+          >
+            Submit
+          </button>
+        </form>
+        <div className="sign-up">
+          <h3>Don't have an account?</h3>
+          <button className="login-buttons">Sign up!</button>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,14 @@
 import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    navigate("/");
+  };
+
   return (
     <div className="links">
       <Link to={"/home"} title="Home">
@@ -11,6 +17,7 @@ export default function Navbar() {
       <Link to={"/browse"} title="Browse Books">
         <button>Browse Books</button>
       </Link>
+      <button onClick={handleSignOut}>Sign Out</button>
     </div>
   );
 }
