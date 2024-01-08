5a98d0d2160cff097afede8961b3cbaa72520adc
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-01-08 12:10:03 -0700

    Added error messages for Contact form.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,6 +8,14 @@ export default function Contact() {
   const [email, setEmail] = useState("");
   const [request, setRequest] = useState("");
 
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+  const [notificationFontColor, setNotificationFontColor] = useState("#991212");
+  const [notificationBackgroundColor, setNotificationBackgroundColor] =
+    useState("#d18282");
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [notification, setNotification] = useState("");
+
   const navigate = useNavigate();
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -16,7 +24,11 @@ export default function Contact() {
     const formElement = event.target as HTMLFormElement;
 
     if (name === "" || request === "" || email === "") {
-      alert("Name, Email, and Request Type are mandatory. Please fill out.");
+      setNotification("Missing Information");
+      setNotificationMessage(
+        "Name, Email, and Request Type are mandatory. Please fill out the form completely."
+      );
+      setIsNotificationOpen(true);
       return;
     }
 
@@ -29,8 +41,25 @@ export default function Contact() {
       )
       .then((result) => {
         console.log(result);
-        alert("Success!");
-        navigate("/");
+        setIsSuccess(true);
+        setIsNotificationOpen(true);
+        setNotificationBackgroundColor("#78fa85");
+        setNotificationFontColor("#17661f");
+        setNotification("Success!");
+        setNotificationMessage(
+          "Email sent successfully! We'll get back to you as soon as possible. You will be redirected to the login screen now."
+        );
+        setTimeout(() => {
+          navigate("/");
+        }, 5000);
+      })
+      .catch((reason) => {
+        console.log(reason);
+        setIsNotificationOpen(true);
+        setNotification("Something went wrong :(");
+        setNotificationMessage(
+          "Whoops! Something went wrong. Please close this window and try again."
+        );
       });
   };
 
@@ -41,6 +70,48 @@ export default function Contact() {
         Use this to get in contact with the site admin to create or recover an
         account.
       </p>
+      {isNotificationOpen ? (
+        <div
+          className="result-message"
+          style={{
+            color: `${notificationFontColor}`,
+            backgroundColor: `${notificationBackgroundColor}`,
+            // position: "absolute",
+            // left: "33%",
+            width: "50%",
+            margin: "auto",
+            marginBottom: "1rem",
+            border: `2px solid ${notificationFontColor}`,
+            borderRadius: `10px`,
+            padding: `1rem`,
+          }}
+        >
+          <h3>{notification}</h3>
+          <p>{notificationMessage}</p>
+          {isSuccess ? (
+            <></>
+          ) : (
+            <button
+              onClick={() => {
+                setIsNotificationOpen(false);
+              }}
+              style={{
+                fontFamily:
+                  "'Dancing Script', cursive, 'Times New Roman', serif",
+                color: "white",
+                backgroundColor: `${notificationFontColor}`,
+                borderRadius: "10px",
+                padding: ".5rem",
+                border: "none",
+              }}
+            >
+              Close
+            </button>
+          )}
+        </div>
+      ) : (
+        <></>
+      )}
       <form id="contact-form" onSubmit={handleSubmit}>
         <label htmlFor="contact-name">Full Name</label>
         <input
